1ec3b88f34fb023403fcd207c668741e98f33244
Your Name <travisjryan@gmail.com>
2013-08-18 20:16:21 -1000

    Removing phone number

--- src/Travisjryan/Twilio/Commands/TwilioSmsCommand.php
+++ src/Travisjryan/Twilio/Commands/TwilioSmsCommand.php
@@ -52,7 +52,7 @@ class TwilioSmsCommand extends Command {
 
         $this->line($text);
 
-        Twilio::message('18085551212', $text);
+        Twilio::message($this->argument('phone'), $text);
 
 
     }
