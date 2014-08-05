e3d0a8e8c6a80a7382fdd3186447b26bab239838
Andy Fleming <andy@andyfleming.com>
2014-08-05 12:02:32 -0700

    Update of config to match Laravel style configs

--- src/config/twilio.php
+++ src/config/twilio.php
@@ -1,18 +1,38 @@
 <?php
 
 return array(
-    /**
-     * SID - Your Twilio Account SID #
-     */
+
+    /*
+    |--------------------------------------------------------------------------
+    | SID
+    |--------------------------------------------------------------------------
+    |
+    | Your Twilio Account SID #
+    |
+    */
+
     "sid" => "",
 
-    /**
-     * token - Access token that can be found in your Twilio dashboard
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Access Token
+    |--------------------------------------------------------------------------
+    |
+    | Access token that can be found in your Twilio dashboard
+    |
+    */
+
     "token" => "",
 
-    /**
-     * from - The Phone number registered with Twilio that your SMS & Calls will come from
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | From Number
+    |--------------------------------------------------------------------------
+    |
+    | The Phone number registered with Twilio that your SMS & Calls will come from
+    |
+    */
+
     "from" => ""
+    
 );
