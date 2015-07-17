5dcc9a5257731766d2fee08b01267ac7b8751de2
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2015-07-17 17:48:30 +0200

    Correct return types in dummy implementation class

--- src/Dummy.php
+++ src/Dummy.php
@@ -7,6 +7,8 @@ class Dummy implements TwilioInterface
      * @param string $to
      * @param string $message
      * @param string $from
+     *
+     * @return \Services_Twilio_Rest_Call|void
      */
     public function message($to, $message, $from = null)
     {
@@ -17,6 +19,8 @@ class Dummy implements TwilioInterface
      * @param string $message
      * @param array $mediaUrls
      * @param string $from
+     *
+     * @return \Services_Twilio_Rest_Call|void
      */
     public function messageWithMedia($to, $message, array $mediaUrls = null, $from = null)
     {
@@ -27,6 +31,8 @@ class Dummy implements TwilioInterface
      * @param string|callable $message
      * @param array $options
      * @param string $from
+     *
+     * @return \Services_Twilio_Rest_Call|void
      */
     public function call($to, $message, array $options = [], $from = null)
     {
