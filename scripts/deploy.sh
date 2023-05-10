39c3083f1270db65dd37464dbcec0b156ee24330
Ronald A. Richardson <me@ron.dev>
2023-05-10 13:55:25 +0800

    latest pending socket addition

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,7 @@ set -ev
 
 php /var/www/html/api/artisan mysql:createdb
 php /var/www/html/api/artisan migrate --force
-php /var/www/html/api/artisan migrate:sandbox --force
-php /var/www/html/api/artisan init:key-column
-php /var/www/html/api/artisan fix:data
+php /var/www/html/api/artisan sandbox:migrate --force
+php /var/www/html/api/artisan sandbox:init-key
 php /var/www/html/api/artisan db:seed --force
 php /var/www/html/api/artisan queue:restart
