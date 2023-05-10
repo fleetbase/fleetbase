2be0f47d6f4fc8da7fa9b4a8a25b5111c3a7efbb
Ronald A. Richardson <me@ron.dev>
2023-05-10 13:44:59 +0800

    latest api base

--- api/config/database.php
+++ api/config/database.php
@@ -2,22 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$host = env('DB_HOST', '127.0.0.1');
-$database = env('DB_DATABASE', 'fleetbase');
-$username = env('DB_USERNAME', 'fleetbase');
-$password = env('DB_PASSWORD', '');
-
-if ($databaseUrl = getenv('DATABASE_URL')) {
-    $url = parse_url($databaseUrl);
-
-    $host = $url['host'];
-    $username = $url['user'];
-    if (isset($url['pass'])) {
-        $password = $url['pass'];
-    }
-    $database = substr($url['path'], 1);
-}
-
 $redis_host = env('REDIS_HOST', '127.0.0.1');
 $redis_database = env('REDIS_DATABASE', '0');
 $redis_password = env('REDIS_PASSWORD', null);
@@ -32,13 +16,9 @@ if ($cacheUrl = getenv('CACHE_URL')) {
     $redis_database = isset($url['path']) ? substr($url['path'], 1) : 'cache';
 }
 
-$mysql_options = [];
-
-if (env('APP_ENV') === 'local') {
-    $mysql_options[PDO::ATTR_EMULATE_PREPARES] = true;
-}
 
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -68,118 +48,7 @@ return [
     |
     */
 
-    'connections' => [
-        'sqlite' => [
-            'driver' => 'sqlite',
-            'url' => env('DATABASE_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
-            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'mysql' => [
-            'driver' => 'mysql',
-            'host' => $host,
-            'port' => env('DB_PORT', '3306'),
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-            'options' => $mysql_options,
-        ],
-
-        'sandbox' => [
-            'driver' => 'mysql',
-            'host' => $host,
-            'port' => env('SANDBOX_DB_PORT', '3306'),
-            'database' => $database . '_sandbox',
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-            'options' => $mysql_options,
-        ],
-
-        'frontend' => [
-            'driver' => 'mysql',
-            'host' => $host,
-            'port' => env('FRONTEND_DB_PORT', '3306'),
-            'database' => $database . '_frontend',
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-            'options' => $mysql_options,
-        ],
-
-        'greenfreight' => [
-            'driver' => 'mysql',
-            'host' => $host,
-            'port' => env('GREENFREIGHT_DB_PORT', '3306'),
-            'database' => $database . '_greenfreight',
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-            'options' => $mysql_options,
-        ],
-
-        'storefront' => [
-            'driver' => 'mysql',
-            'host' => $host,
-            'port' => env('STOREFRONT_DB_PORT', '3306'),
-            'database' => $database . '_storefront',
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
-            'options' => $mysql_options,
-        ],
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8mb4',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
-        'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8mb4',
-            'prefix' => '',
-            'prefix_indexes' => true,
-        ],
-    ],
+    'connections' => [],
 
     /*
     |--------------------------------------------------------------------------
@@ -241,4 +110,5 @@ return [
             'database' => $redis_database . '_geocode_cache',
         ],
     ],
+
 ];
