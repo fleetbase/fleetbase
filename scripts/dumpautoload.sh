ae5fb66e5b3587548ecda966e6212af0419fbe79
Ronald A. Richardson <me@ron.dev>
2023-05-30 16:36:33 +0800

    latest

--- scripts/dumpautoload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-composer dump-autoload --working-dir /var/www/html/api
-composer dump-autoload --working-dir /var/www/html/packages/core-api
-composer dump-autoload --working-dir /var/www/html/packages/fleetops-api
-composer dump-autoload --working-dir /var/www/html/packages/storefront-api
