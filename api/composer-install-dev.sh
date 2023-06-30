80c621d020b9c3ae395f295138d58271fa630f18
Ronald A. Richardson <me@ron.dev>
2023-06-30 11:36:14 +0800

    Added Branding Settings to Admin section

--- api/composer-install-dev.sh
+++ api/composer-install-dev.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+if [ -f composer.lock ]; then
+    rm composer.lock
+fi
+
 mv composer.json composer.prod.json && mv composer.dev.json composer.json && composer install && mv composer.json composer.dev.json && mv composer.prod.json composer.json
