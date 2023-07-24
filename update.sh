b5dca06a7062863b9cbad4601f4225d86181e8ac
Ronald A. Richardson <me@ron.dev>
2023-07-24 18:05:06 +0800

    includes small update to retain mysql data locally, also includes update script

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Exit the script as soon as a command fails
+set -e
+
+echo "Switching to the main branch..."
+git checkout main
+
+echo "Updating submodules..."
+git submodule update --init --recursive
+
+echo "Updating console..."
+cd console
+git checkout main
+git pull
+cd ..
+
+echo "Building Docker images..."
+docker-compose build console
+docker-compose build application
+
+echo "Update completed. Run \`docker-compose up -d\` to launch!"
