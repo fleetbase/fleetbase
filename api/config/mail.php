20c0c2127cd7177142bda47383e1b996204adc7a
Ronald A. Richardson <me@ron.dev>
2023-08-01 21:02:15 +0800

    set correct default config options

--- api/config/mail.php
+++ api/config/mail.php
@@ -96,8 +96,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@fleetbase.io'),
+        'name' => env('MAIL_FROM_NAME', 'Fleetbase'),
     ],
 
     /*
