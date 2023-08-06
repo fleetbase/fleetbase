7b8b74422e9619c39254beac885d3faeed516f85
Ronald A. Richardson <me@ron.dev>
2023-08-06 14:30:58 +0800

    Upgraded `fleetbase/core-api` - Prevent mergeConfigFromSettings() from overwriting ENV variables

--- api/config/filesystems.php
+++ api/config/filesystems.php
@@ -56,7 +56,6 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_BUCKET_ENDPOINT', env('AWS_ENDPOINT')),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
