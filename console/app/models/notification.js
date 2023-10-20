4fb2dec8c3ae628a6b0fa53a1cdb3a7818614d50
Temuulen Bayanmunkh <taka@Temuulens-MacBook-Pro.local>
2023-10-20 18:06:43 +0800

    Added notification tray component

---
+++ console/app/models/notification.js
@@ -0,0 +1,25 @@
+import Model, { attr } from '@ember-data/model';
+import { computed } from '@ember/object';
+import { format, formatDistanceToNow } from 'date-fns';
+
+export default class NotificationModel extends Model {
+    @attr('string') notifiable_id;
+    @attr('string') notifiable_type;
+
+    /** @attributes */
+    @attr('string') type;
+    @attr('raw') data;
+
+    /** @dates */
+    @attr('date') read_at;
+    @attr('date') created_at;
+
+    /** @computed */
+    @computed('created_at') get createdAgo() {
+        return formatDistanceToNow(this.created_at);
+    }
+
+    @computed('created_at') get createdAt() {
+        return format(this.created_at, 'PPP p');
+    }
+}
---
+++ console/app/serializers/notification.js
@@ -0,0 +1,5 @@
+import ApplicationSerializer from '@fleetbase/ember-core/serializers/application';
+
+export default class NotificationSerializer extends ApplicationSerializer {
+    primaryKey = 'id';
+}
