41761ea50e1805c6bd0016e76215de063b21046b
Ronald A. Richardson <me@ron.dev>
2024-01-23 21:20:51 +0800

    add delay to extensions initialization step

--- console/app/instance-initializers/load-extensions.js
+++ console/app/instance-initializers/load-extensions.js
@@ -1,17 +1,24 @@
 import loadExtensions from '@fleetbase/ember-core/utils/load-extensions';
+import { later } from '@ember/runloop';
 
 export function initialize(owner) {
-    const universe = owner.lookup('service:universe');
+    later(
+        this,
+        () => {
+            const universe = owner.lookup('service:universe');
 
-    loadExtensions().then((extensions) => {
-        extensions.forEach((extension) => {
-            universe.loadEngine(extension.name).then((engineInstance) => {
-                if (engineInstance.base && engineInstance.base.setupExtension) {
-                    engineInstance.base.setupExtension(owner, engineInstance, universe);
-                }
+            loadExtensions().then((extensions) => {
+                extensions.forEach((extension) => {
+                    universe.loadEngine(extension.name).then((engineInstance) => {
+                        if (engineInstance.base && engineInstance.base.setupExtension) {
+                            engineInstance.base.setupExtension(owner, engineInstance, universe);
+                        }
+                    });
+                });
             });
-        });
-    });
+        },
+        600
+    );
 }
 
 export default {
