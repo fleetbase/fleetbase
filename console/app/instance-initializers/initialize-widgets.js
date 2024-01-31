1d92764958c6709111fb12cb072e06f9a857b2c8
Turtuvshin <toroo.byamba@gmail.com>
2024-01-31 15:56:39 +0800

    Add dashboard metric component and initialize widgets

--- console/app/instance-initializers/initialize-widgets.js
+++ console/app/instance-initializers/initialize-widgets.js
@@ -1,51 +1,6 @@
 export function initialize(application) {
     const universe = application.lookup('service:universe');
 
-    const fleetOpsWidgetConfigs = [
-        { name: 'Earnings', format: 'money', currency: 'USD' },
-        { name: 'Fuel costs', format: 'money', currency: 'USD' },
-        { name: 'Total distance traveled', format: 'meters' },
-        { name: 'Orders canceled' },
-        { name: 'Orders completed' },
-        { name: 'Orders in progress' },
-        { name: 'Orders scheduled' },
-        { name: 'Drivers online' },
-        { name: 'Total drivers' },
-        { name: 'Open Issues' },
-        { name: 'Total customers' },
-        { name: 'Open Issues' },
-        { name: 'Resolved Issues' },
-    ];
-
-    const storefrontWidgetConfigs = [
-        { name: 'Total Products' },
-        { name: 'Total Stores' },
-        { name: 'Total Networks' },
-        { name: 'Ordser in progress' },
-        { name: 'Orders completed' },
-        { name: 'Orders canceled' },
-    ];
-
-    const iamWidgetConfigs = [{ name: 'User count' }, { name: 'Group count' }, { name: 'Policy count' }, { name: 'Role count' }];
-
-    const createWidgets = (configs) => configs.map(({ name, format, currency }) => createWidget(name, format, currency));
-
-    const createWidget = (name, format, currency) => ({
-        name,
-        component: 'dashboard/count',
-        grid_options: { w: 2, h: 2 },
-        options: {
-            title: name,
-            value: 0,
-            ...(format && { format }),
-            ...(currency && { currency }),
-        },
-    });
-
-    const fleetOpsWidgets = createWidgets(fleetOpsWidgetConfigs);
-    const storefrontWidgets = createWidgets(storefrontWidgetConfigs);
-    const iamWidgets = createWidgets(iamWidgetConfigs);
-
     const availableWidgets = [
         {
             name: 'Fleetbase Blog',
@@ -63,9 +18,33 @@ export function initialize(application) {
                 title: 'Github Card',
             },
         },
-        ...fleetOpsWidgets,
-        ...storefrontWidgets,
-        ...iamWidgets,
+        {
+            name: 'Fleet-Ops Metrics',
+            component: 'dashboard/metric',
+            grid_options: { w: 12, h: 4 },
+            options: {
+                title: 'Fleet-Ops Metrics',
+                endpoint: 'int/v1/fleet-ops/dashboard',
+            },
+        },
+        {
+            name: 'Identity & Access Management Metrics',
+            component: 'dashboard/metric',
+            grid_options: { w: 12, h: 3 },
+            options: {
+                title: 'Identity & Access Management Metrics',
+                endpoint: 'int/v1/metrics/iam-dashboard',
+            },
+        },
+        {
+            name: 'Storefront Metrics',
+            component: 'dashboard/metric',
+            grid_options: { w: 12, h: 3 },
+            options: {
+                title: 'Storefront Metrics',
+                endpoint: 'storefront/int/v1/dashboard',
+            },
+        },
     ];
 
     universe.registerWidgets(availableWidgets);
