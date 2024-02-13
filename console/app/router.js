797a3d61feed89d350aafee111e0973fcea9b662
Ronald A. Richardson <me@ron.dev>
2024-02-13 13:55:57 +0800

    fix router

--- console/app/router.js
+++ console/app/router.js
@@ -55,31 +55,6 @@ Router.map(function () {
                 });
             });
         });
-
-        this.mount('@fleetbase/billing-engine', {
-            as: 'billing',
-            path: 'billing'
-        });
-
-        this.mount('@fleetbase/dev-engine', {
-            as: 'developers',
-            path: 'developers'
-        });
-
-        this.mount('@fleetbase/fleetops-engine', {
-            as: 'fleet-ops',
-            path: 'fleet-ops'
-        });
-
-        this.mount('@fleetbase/iam-engine', {
-            as: 'iam',
-            path: 'iam'
-        });
-
-        this.mount('@fleetbase/storefront-engine', {
-            as: 'storefront',
-            path: 'storefront'
-        });
     });
     this.route('install');
 });
