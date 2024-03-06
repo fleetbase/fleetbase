79604c7981108909abacfc90d2ce1d83cba7f0ac
Ronald A. Richardson <me@ron.dev>
2024-03-06 13:10:31 +0800

    preparing for v0.4.10 release

--- console/app/initializers/load-socketcluster-client.js
+++ console/app/initializers/load-socketcluster-client.js
@@ -1,7 +1,12 @@
 export function initialize() {
-    const socketClusterClientScript = document.createElement('script');
-    socketClusterClientScript.src = '/assets/socketcluster-client.min.js';
-    document.body.appendChild(socketClusterClientScript);
+    // Check if the script already exists
+    // Only insert the script tag if it doesn't already exist
+    if (!document.querySelector('script[data-socketcluster-client]')) {
+        const socketClusterClientScript = document.createElement('script');
+        socketClusterClientScript.setAttribute('data-socketcluster-client', '1');
+        socketClusterClientScript.src = '/assets/socketcluster-client.min.js';
+        document.body.appendChild(socketClusterClientScript);
+    }
 }
 
 export default {
--- console/config/environment.js
+++ console/config/environment.js
@@ -46,9 +46,12 @@ module.exports = function (environment) {
             driverImage: getenv('DEFAULT_DRIVER_IMAGE', 'https://s3.ap-southeast-1.amazonaws.com/flb-assets/static/no-avatar.png'),
             userImage: getenv('DEFAULT_USER_IMAGE', 'https://s3.ap-southeast-1.amazonaws.com/flb-assets/static/no-avatar.png'),
             contactImage: getenv('DEFAULT_CONTACT_IMAGE', 'https://s3.ap-southeast-1.amazonaws.com/flb-assets/static/no-avatar.png'),
+            entityImage: getenv('DEFAULT_ENTITY_IMAGE', 'https://flb-assets.s3-ap-southeast-1.amazonaws.com/static/parcels/medium.png'),
             vendorImage: getenv('DEFAULT_VENDOR_IMAGE', 'https://s3.ap-southeast-1.amazonaws.com/flb-assets/static/no-avatar.png'),
             vehicleImage: getenv('DEFAULT_VEHICLE_IMAGE', 'https://s3.ap-southeast-1.amazonaws.com/flb-assets/static/vehicle-placeholder.png'),
             vehicleAvatar: getenv('DEFAUL_VEHICLE_AVATAR', 'https://flb-assets.s3-ap-southeast-1.amazonaws.com/static/vehicle-icons/mini_bus.svg'),
+            driverAvatar: getenv('DEFAUL_DRIVER_AVATAR', 'https://flb-assets.s3-ap-southeast-1.amazonaws.com/static/driver-icons/moto-driver.png'),
+            placeAvatar: getenv('DEFAUL_PLACE_AVATAR', 'https://flb-assets.s3-ap-southeast-1.amazonaws.com/static/place-icons/basic-building.png'),
         },
 
         'ember-simple-auth': {
