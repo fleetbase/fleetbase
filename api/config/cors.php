20a1793447eeaf607b7fc67f9e0daaa82c11c309
Ronald A. Richardson <me@ron.dev>
2024-03-11 21:07:23 +0800

    remove test url from cors `allowed_origins`

--- api/config/cors.php
+++ api/config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(['http://localhost:4200', 'https://flbconsole.jp.ngrok.io', env('CONSOLE_HOST'), Utils::addWwwToUrl(env('CONSOLE_HOST'))]),
+    'allowed_origins' => array_filter(['http://localhost:4200', env('CONSOLE_HOST'), Utils::addWwwToUrl(env('CONSOLE_HOST'))]),
 
     'allowed_origins_patterns' => [],
 
