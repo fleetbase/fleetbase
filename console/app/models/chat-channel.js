4191d6a0bba6b4579492883d1058ea56021dcff3
Ronald A. Richardson <me@ron.dev>
2024-04-03 15:20:25 +0800

    added computed date properties to ChatChannelModel

--- console/app/models/chat-channel.js
+++ console/app/models/chat-channel.js
@@ -1,26 +1,58 @@
 import Model, { attr, hasMany } from '@ember-data/model';
 import { computed } from '@ember/object';
-import { slugify } from 'ember-string';
+import { format, formatDistanceToNow } from 'date-fns';
 
 export default class ChatChannel extends Model {
-  /** @attributes */
-  @attr('string') name;
-  @attr('string') slug;
-  @attr meta;
+    /** @ids */
+    @attr('string') public_id;
+    @attr('string') company_uuid;
 
-  /** @relationships */
-  @hasMany('chat-participant', { async: true }) participants;
-  @hasMany('chat-message', { async: true }) messages;
-  @hasMany('chat-attachment', { async: true }) attachments;
-  @hasMany('chat-presence', { async: true }) presences;
+    /** @attributes */
+    @attr('string') name;
+    @attr('string') slug;
+    @attr('array') meta;
 
-  /** @computed */
-  @computed('name') get slugifiedName() {
-    return slugify(this.name);
-  }
+    /** @relationships */
+    @hasMany('chat-participant', { async: true }) participants;
+    @hasMany('chat-message', { async: true }) messages;
+    @hasMany('chat-attachment', { async: true }) attachments;
+    @hasMany('chat-presence', { async: true }) presences;
 
-  /** @dates */
-  @attr('date') created_at;
-  @attr('date') updated_at;
-  @attr('date') deleted_at;
+    /** @dates */
+    @attr('date') created_at;
+    @attr('date') updated_at;
+    @attr('date') deleted_at;
+
+    /** @computed */
+    @computed('updated_at') get updatedAgo() {
+        if (!isValidDate(this.updated_at)) {
+            return null;
+        }
+
+        return formatDistanceToNow(this.updated_at);
+    }
+
+    @computed('updated_at') get updatedAt() {
+        if (!isValidDate(this.updated_at)) {
+            return null;
+        }
+
+        return formatDate(this.updated_at, 'PP HH:mm');
+    }
+
+    @computed('created_at') get createdAgo() {
+        if (!isValidDate(this.created_at)) {
+            return null;
+        }
+
+        return formatDistanceToNow(this.created_at);
+    }
+
+    @computed('created_at') get createdAt() {
+        if (!isValidDate(this.created_at)) {
+            return null;
+        }
+
+        return formatDate(this.created_at, 'PP HH:mm');
+    }
 }
