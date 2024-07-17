579a3698882bcaef226f6ebb0ef3e175e4781eb0
Ronald A. Richardson <me@ron.dev>
2024-07-18 01:48:55 +0800

    preparing arch change for extensions implementation

--- console/app/instance-initializers/load-extensions.js
+++ console/app/instance-initializers/load-extensions.js
@@ -1,17 +1,8 @@
-import loadExtensions from '@fleetbase/ember-core/utils/load-extensions';
-
-export function initialize(owner) {
+export function initialize (owner) {
     const universe = owner.lookup('service:universe');
-
-    loadExtensions().then((extensions) => {
-        extensions.forEach((extension) => {
-            universe.loadEngine(extension.name).then((engineInstance) => {
-                if (engineInstance.base && engineInstance.base.setupExtension) {
-                    engineInstance.base.setupExtension(owner, engineInstance, universe);
-                }
-            });
-        });
-    });
+    if (universe) {
+        universe.bootEngines(owner);
+    }
 }
 
 export default {
