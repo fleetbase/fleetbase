d84f4792fa8390250c17cb3afb23209a193f3d6c
thangasujithas <144225567+thangasujithaacs@users.noreply.github.com>
2025-01-23 14:53:10 +0530

    otp content translation string added

---
+++ api/resources/lang/it/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'otp_content' => 'Benvenuto su FleetYes. Il tuo codice di verifica è: :code',
+    'otp_message' => 'Il tuo codice di verifica per :company è :code',
+    'no_driver_found' => 'Nessun autista trovato con questo numero di telefono.',
+    'sms_verification_error' => 'Impossibile inviare il codice di verifica tramite SMS.',
+];
