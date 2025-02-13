eae4fa9ed28d86b08f0b482df93249bad107b641
thangasujithas <144225567+thangasujithaacs@users.noreply.github.com>
2025-02-13 17:28:29 +0530

    amount & currency format issue fixed

--- packages/ember-ui/addon/utils/format-currency.js
+++ packages/ember-ui/addon/utils/format-currency.js
@@ -3,6 +3,12 @@ import formatMoney from '@fleetbase/ember-accounting/utils/format-money';
 
 export default function formatCurrency(amount = 0, currencyCode = 'USD') {
     let currency = getCurrency(currencyCode);
-
-    return formatMoney(!currency.decimalSeparator ? amount : amount / 100, currency.symbol, currency.precision, currency.thousandSeparator, currency.decimalSeparator);
+    // return formatMoney(!currency.decimalSeparator ? amount : amount / 100, currency.symbol, currency.precision, currency.thousandSeparator, currency.decimalSeparator);
+    return formatMoney(
+        amount, // Only divide when necessary
+        currency.symbol,
+        currency.precision,
+        currency.thousandSeparator,
+        currency.decimalSeparator
+    );
 }
