9ed75612577148cb1d890b47590578f07bb56aae
ChrisHanshia16 <hanshia@agilecybersolutions.com>
2025-03-31 08:00:47 +0530

    fix : phone number issue fix

--- packages/ember-ui/addon/components/phone-input.js
+++ packages/ember-ui/addon/components/phone-input.js
@@ -7,12 +7,11 @@ import intlTelInput from 'intl-tel-input';
 export default class PhoneInputComponent extends Component {
     @service fetch;
     @tracked iti;
-    @tracked displayValue = '+';
 
     @action setupIntlTelInput(element) {
         this.iti = intlTelInput(element, {
             containerClass: `w-full ${this.args.wrapperClass ?? ''}`,
-            initialCountry: 'auto',
+            initialCountry: 'gb',
             separateDialCode: true,
             formatAsYouType: true,
             geoIpLookup: (success, failure) => {
@@ -33,71 +32,24 @@ export default class PhoneInputComponent extends Component {
         element.addEventListener('countrychange', this.args.onCountryChange);
     }
 
-    @action onInput(event) {
-        let number = event.target.value;
+    @action onInput() {
+        const { onInput } = this.args;
+        const number = this.iti.getNumber(intlTelInput.utils.numberFormat.E164);
 
-        // Ensure the number always starts with "+"
-        if (!number.startsWith('+')) {
-            number = `+${number.replace(/[^0-9]/g, '')}`;
-        }
-
-        this.displayValue = number;
-
-        if (typeof this.args.onInput === 'function') {
-            this.args.onInput(number);
+        if (typeof onInput === 'function') {
+            onInput(number, ...arguments);
         }
     }
 
-    @action preventPlusRemoval(event) {
-        // Prevent deleting "+" character
-        if (event.key === 'Backspace' && event.target.selectionStart <= 1) {
-            event.preventDefault();
+    @action handleInput(event) {
+        let inputValue = event.target.value;
+
+        // Ensure the '+' sign is always at the start
+        if (!inputValue.startsWith('+')) {
+            inputValue = '+44' + inputValue.replace(/[^0-9]/g, ''); // Keep only numeric values
         }
+
+        // Update the input value without triggering infinite loops
+        event.target.value = inputValue;
     }
 }
-
-
-
-// import Component from '@glimmer/component';
-// import { tracked } from '@glimmer/tracking';
-// import { inject as service } from '@ember/service';
-// import { action } from '@ember/object';
-// import intlTelInput from 'intl-tel-input';
-
-// export default class PhoneInputComponent extends Component {
-//     @service fetch;
-//     @tracked iti;
-
-//     @action setupIntlTelInput(element) {
-//         this.iti = intlTelInput(element, {
-//             containerClass: `w-full ${this.args.wrapperClass ?? ''}`,
-//             initialCountry: 'auto',
-//             separateDialCode: true,
-//             formatAsYouType: true,
-//             geoIpLookup: (success, failure) => {
-//                 this.fetch
-//                     .get('lookup/whois')
-//                     .then((response) => {
-//                         success(response.country_code);
-//                     })
-//                     .catch(failure);
-//             },
-//             utilsScript: '/assets/libphonenumber/utils.js',
-//         });
-
-//         if (typeof this.args.onInit === 'function') {
-//             this.args.onInit(this.iti);
-//         }
-
-//         element.addEventListener('countrychange', this.args.onCountryChange);
-//     }
-
-//     @action onInput() {
-//         const { onInput } = this.args;
-//         const number = this.iti.getNumber(intlTelInput.utils.numberFormat.E164);
-
-//         if (typeof onInput === 'function') {
-//             onInput(number, ...arguments);
-//         }
-//     }
-// }
