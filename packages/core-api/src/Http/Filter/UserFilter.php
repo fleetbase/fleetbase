c6c249400b66759674a163a6750d691ffcbe81ae
thangasujithas <144225567+thangasujithaacs@users.noreply.github.com>
2025-05-08 18:17:00 +0530

    Fix:500 Error in Console When Using Roles Filter

--- packages/core-api/src/Http/Filter/UserFilter.php
+++ packages/core-api/src/Http/Filter/UserFilter.php
@@ -51,8 +51,21 @@ class UserFilter extends Filter
 
     public function role(?string $roleId)
     {
-        $this->builder->whereHas('roles', function ($query) use ($roleId) {
-            $query->where('id', $roleId);
+        if (!$roleId) {
+            return;
+        }
+
+        $this->builder->whereExists(function ($query) use ($roleId) {
+            $query->selectRaw(1)
+                ->from('model_has_roles')
+                ->join('company_users', 'model_has_roles.model_uuid', '=', 'company_users.uuid')
+                ->whereColumn('company_users.user_uuid', 'users.uuid')
+                ->where('model_has_roles.model_type', '=', 'Fleetbase\Models\CompanyUser')
+                ->where('model_has_roles.role_id', $roleId);
+
+            if ($companyUuid = $this->session->get('company')) {
+                $query->where('company_users.company_uuid', $companyUuid);
+            }
         });
     }
 }
