6697b791858ea2d96d185dca15e7c24f1d8f98a4
Ronald A. Richardson <me@ron.dev>
2025-05-19 13:50:10 +0800

    secured runtime config to only allow select config values to be set

--- console/app/app.js
+++ console/app/app.js
@@ -4,25 +4,7 @@ import loadInitializers from 'ember-load-initializers';
 import config from '@fleetbase/console/config/environment';
 import loadExtensions from '@fleetbase/ember-core/utils/load-extensions';
 import mapEngines from '@fleetbase/ember-core/utils/map-engines';
-import { set } from '@ember/object';
-import { debug } from '@ember/debug';
-
-async function loadRuntimeConfig() {
-    try {
-        const response = await fetch(`/fleetbase.config.json?_t=${Date.now()}`, { cache: 'no-cache' });
-        if (!response.ok) {
-            debug('No fleetbase.config.json found, using built-in config defaults');
-            return;
-        }
-
-        const runtimeConfig = await response.json();
-        for (const [path, value] of Object.entries(runtimeConfig)) {
-            set(config, path, value);
-        }
-    } catch (e) {
-        debug(`Failed to load runtime config : ${e.message}`);
-    }
-}
+import loadRuntimeConfig from '@fleetbase/console/utils/runtime-config';
 
 export default class App extends Application {
     modulePrefix = config.modulePrefix;
