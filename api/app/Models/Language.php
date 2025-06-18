70d531293c800124d2785b6e3d00e51dac1582ab
thangasujithas <144225567+thangasujithaacs@users.noreply.github.com>
2025-06-18 17:12:03 +0530

    Fix: language not found issue fix

--- api/app/Models/Language.php
+++ api/app/Models/Language.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Language extends Model
 {
-    use SoftDeletes;
 
     protected $table = 'languages';
 
