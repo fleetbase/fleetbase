03041a727bf684e2146e579d4e54fd2571dce1ef
thangasujithas <144225567+thangasujithaacs@users.noreply.github.com>
2025-07-28 14:12:59 +0530

    Fix: place removed after edit order routes

--- packages/fleetops-api/server/src/Http/Resources/v1/WaypointDirect.php
+++ packages/fleetops-api/server/src/Http/Resources/v1/WaypointDirect.php
@@ -46,6 +46,7 @@ class WaypointDirect extends FleetbaseResource
             'status'                 => $this->getWaypointStatus($waypoint),
             'status_code'            => $this->getWaypointStatusCode($waypoint),
             'place_public_id'        => $place ? $place->public_id : null,
+            'place_uuid'             => $place ? $place->uuid : null,
             // Place-related data (if place exists)
             'name'                   => $place ? ($locale ? $locationService->translateLocation($place->name, $locale) : $place->name) : null,
             'location'               => $place ? ($place->location ?? new Point(0, 0)) : new Point(0, 0),
