22196b98c60dc41729f2e634a974dc38c5d89ef0
kalaivani <kalaivani@agilecybersolutions.com>
2025-08-15 18:24:24 +0530

    Fix: modal process after file import successfull

--- packages/fleetops-engine/addon/controllers/management/fleets/index.js
+++ packages/fleetops-engine/addon/controllers/management/fleets/index.js
@@ -455,8 +455,15 @@ export default class ManagementFleetsIndexController extends BaseController {
                 handleSuccessfulImport(this, results, modal, this.onImportSuccess.bind(this));
 
             } catch (error) {
-                console.error('Fleet import error:', error);
+                console.error('Import failed:', error);
                 modal.stopLoading();
+                this.modalsManager.setOption('isErrorState', false);
+                this.modalsManager.setOption('errorLogUrl', null);
+                this.modalsManager.setOption('uploadQueue', []);
+                this.modalsManager.setOption('acceptButtonText', this.intl.t('fleet-ops.component.modals.order-import.start-upload-button'));
+                this.modalsManager.setOption('acceptButtonIcon', 'upload');
+                // this.modalsManager.setOption('acceptButtonScheme', 'magic');
+                this.modalsManager.setOption('acceptButtonDisabled', true);
                 this.modalsManager.setOption('isProcessing', false);
                 this.notifications.serverError(error);
             }
--- packages/fleetops-engine/addon/controllers/management/places/index.js
+++ packages/fleetops-engine/addon/controllers/management/places/index.js
@@ -780,6 +780,13 @@ export default class ManagementPlacesIndexController extends BaseController {
             } catch (error) {
                 console.error('Import failed:', error);
                 modal.stopLoading();
+                this.modalsManager.setOption('isErrorState', false);
+                this.modalsManager.setOption('errorLogUrl', null);
+                this.modalsManager.setOption('uploadQueue', []);
+                this.modalsManager.setOption('acceptButtonText', this.intl.t('fleet-ops.component.modals.order-import.start-upload-button'));
+                this.modalsManager.setOption('acceptButtonIcon', 'upload');
+                // this.modalsManager.setOption('acceptButtonScheme', 'magic');
+                this.modalsManager.setOption('acceptButtonDisabled', true);
                 this.modalsManager.setOption('isProcessing', false);
                 this.notifications.serverError(error);
             }
