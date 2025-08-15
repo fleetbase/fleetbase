0314d3b1d9c6c1df8229770ff978601f211e488c
thangasujithas <144225567+thangasujithaacs@users.noreply.github.com>
2025-08-15 15:51:07 +0530

    Fix: Vehicle Required field validations added

--- packages/fleetops-api/server/src/Http/Controllers/Internal/v1/VehicleController.php
+++ packages/fleetops-api/server/src/Http/Controllers/Internal/v1/VehicleController.php
@@ -333,10 +333,26 @@ class VehicleController extends FleetOpsController
         $plateNumber = $this->getVehicleValue($row, ['plate_number', 'license_plate', 'license_place_number', 'vehicle_plate', 'registration_plate', 'tag_number', 'tail_number', 'head_number']);
 
         // Basic validation - make is required (or vehicle name which can be parsed for make)
-        if (empty($make) && empty($vehicleName)) {
+        if (empty($make)) {
             $errors[] = [
                 (string)$displayRowIndex,
-                "Vehicle make or vehicle name is required.",
+                "Vehicle make is required.",
+                ""
+            ];
+            $hasValidationErrors = true;
+        }
+        if (empty($model)) {
+            $errors[] = [
+                (string)$displayRowIndex,
+                "Vehicle model is required.",
+                ""
+            ];
+            $hasValidationErrors = true;
+        }
+        if (empty($plateNumber)) {
+            $errors[] = [
+                (string)$displayRowIndex,
+                "Vehicle plate number is required.",
                 ""
             ];
             $hasValidationErrors = true;
