d67b614aeb5e0328beafa33d1ead3c01de8ca2ad
thangasujithaacs <thangasujitha@agilecyber.com>
2025-08-21 11:38:58 +0530

    feat: add sample environment configuration and update CORS settings to include AUTO_ALLOCATION_HOST

--- api/config/cors.php
+++ api/config/cors.php
@@ -21,7 +21,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => array_filter(['http://localhost:4200', env('CONSOLE_HOST'), Utils::addWwwToUrl(env('CONSOLE_HOST'))]),
+    'allowed_origins' => array_filter(['http://localhost:4200', env('CONSOLE_HOST'), Utils::addWwwToUrl(env('CONSOLE_HOST')),  env('AUTO_ALLOCATION_HOST')]),
 
     'allowed_origins_patterns' => [],
 
