3de9b6fccbde5aeb08f363049f6834eb611cf295
encoderuz <abduqodirxojiyev@gmail.com>
2026-02-04 10:35:16 +0500

    Added Uzbek translations for API and Console

---
+++ api/resources/lang/uz/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ushbu hisobga olish maʼlumotlari bizning yozuvlarimizga mos kelmadi.',
+    'password' => 'Taqdim etilgan parol noto‘g‘ri.',
+    'throttle' => 'Kirishga urinishlar juda koʻp. Iltimos, :soniyadan keyin qayta urinib koʻring.',
+
+];
