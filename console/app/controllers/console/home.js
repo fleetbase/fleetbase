d197d2fd26cc182cf5f9107e8451cae7e271b943
Ronald A. Richardson <me@ron.dev>
2026-03-26 17:38:34 +0800

    revert console home controller

--- console/app/controllers/console/home.js
+++ console/app/controllers/console/home.js
@@ -1,8 +1,3 @@
 import Controller from '@ember/controller';
-import { tracked } from '@glimmer/tracking';
-import { inject as service } from '@ember/service';
 
-export default class ConsoleHomeController extends Controller {
-    @service store;
-    @tracked template = this.store.createRecord('template');
-}
+export default class ConsoleHomeController extends Controller {}
